ce133520272899719ce168fe4f3f6163a935ffe6
WeryZebra-Yue <20se02ce035@ppsu.ac.in>
2021-07-24 21:56:27 +0530

    third try

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,29 @@ import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
 import HeaderIcon from '../components/HeaderIcon'
-import Login from './Login'
+// import Login from './Login'
 import Sidebar from '../components/Sidebar'
 import { db ,rdb } from '../firebasee'
+import Image from 'next/image'
+import {signin} from 'next-auth/client'
 
 export default function Home({session}) {
 
   const [sessions] = useSession();
 
-  if(!session) return <Login/>;
+  if(!session) return (
+    <div className="grid place-items-center ">
+            
+    <Image
+    src = "https://links.papareact.com/t4i"
+    height = {400}
+    width = {400}
+    objectFit = "contain"
+    />
+    <h1 onClick={signin} className="p-5 cursor-pointer bg-blue-600 text-white rounded-full text-center font-semibold text-lg"> Log In with Google</h1>
+
+</div>
+  );
   
    rdb.ref("users").child(`${sessions.user.email.split("@")[0]}`).set({
    
