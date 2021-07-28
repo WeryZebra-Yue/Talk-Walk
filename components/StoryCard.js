3c7450018bbfad260ddaec05f2d247cea38ba54a
WeryZebra-Yue <20se02ce035@ppsu.ac.in>
2021-07-28 18:51:06 +0530

    stories2

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -19,10 +19,10 @@ function StoryCard({name,profile,src}) {
          objectFit="cover"
          draggable="false"
          />
-            <p className="absolute bottom-0 opacity-0 group-hover:opacity-100 z-50 p-3 text-gray-800 font-bold text-[.60rem] lg:text-base ">Ayush Master</p>
+            <p className="absolute bottom-0 opacity-0 group-hover:opacity-100 z-50 p-2 text-gray-800 font-bold text-[.70rem] lg:text-base ">Ayush Master</p>
  
          <Image
-         className="object-cover filter rounded-3xl brightness-75 z-0 transition duration-100 hover:brightness-90"
+         className="object-cover filter rounded-3xl brightness-50 z-0 transition duration-100 group-hover:brightness-100"
          src={src}
          layout="fill"
          draggable="false"/>
