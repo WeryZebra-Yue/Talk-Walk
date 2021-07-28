0dd22e740e40324545f1daf8a36db726b3752b7a
WeryZebra-Yue <20se02ce035@ppsu.ac.in>
2021-07-28 19:26:13 +0530

    stories3

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -8,7 +8,7 @@ function StoryCard({name,profile,src}) {
         <div className="relative  h-32 w-20 lg:h-56 lg:w-32  rounded-3xl bg-gray-300 cursor-pointer overflow-x  transition duration-200 transform ease-in hover:scale-105  group" onClick={seeStory}>
             {
          src &&
-        <div className="relative h-32 w-20 lg:h-56 lg:w-32 p-1 unselectable " >
+        <div className="relative h-32 w-20 lg:h-56 lg:w-32  unselectable flex flex-col" >
          
              <Image
          className="opacity-0 absolute lg:opacity-100 rounded-full z-50 group-hover:opacity-0"
@@ -19,7 +19,7 @@ function StoryCard({name,profile,src}) {
          objectFit="cover"
          draggable="false"
          />
-            <p className="absolute bottom-0 opacity-0 group-hover:opacity-100 z-50 p-2 text-gray-800 font-bold text-[.70rem] lg:text-base ">{name}</p>
+            <p className="absolute bottom-0 text-center rounded-b-3xl border-t-2 pb-1 border-gray-400 rounded-md w-full opacity-0 group-hover:opacity-100 z-50 bg-white text-gray-800 font-bold text-[10px] lg:text-base ">{name}</p>
  
          <Image
          className="object-cover filter rounded-3xl brightness-50 z-0 transition duration-100 group-hover:brightness-100"
