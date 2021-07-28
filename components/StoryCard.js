5ca6983b222ffc1c4933ffd3405f3a1ba42781fd
WeryZebra-Yue <20se02ce035@ppsu.ac.in>
2021-07-28 19:31:37 +0530

    s

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -19,7 +19,7 @@ function StoryCard({name,profile,src}) {
          objectFit="cover"
          draggable="false"
          />
-            <p className="absolute bottom-0 text-center rounded-b-3xl border-t-2 pb-1 border-gray-400 rounded-md w-full opacity-0 group-hover:opacity-100 z-50 bg-white text-gray-800 font-bold text-[10px] lg:text-base ">{name}</p>
+            <p className="absolute bottom-0 text-center rounded-b-3xl border-t-2 pb-1 border-gray-400 w-full opacity-0 group-hover:opacity-100 z-50 bg-white text-gray-800 font-bold text-[10px] lg:text-base ">{name}</p>
  
          <Image
          className="object-cover filter rounded-3xl brightness-50 z-0 transition duration-100 group-hover:brightness-100"
